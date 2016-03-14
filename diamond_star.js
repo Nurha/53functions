e05a02d517d0f39076ac130e4d501b543553e241
Nurha <nurha@projectcodex.co>
2016-03-14 15:10:51 +0200

    done

--- diamond_star.js
+++ diamond_star.js
@@ -1,4 +1,26 @@
 function diamond_star(character){
+  var string = '\n';
+  // top of diamond
+  for(x=1; x<=5 ;x++){
+    for(y=1; y<=5-x; y++){
+      string += ' ';
+    };
+    for(y=1; y<=x; y++){
+        string += character + ' ';
+    };
+    string += '\n';
+  };
+// bottom of diamond
+  for(x=5-1; x>=1; x--){
+    for(y=1; y<=5-x; y++){
+      string += ' ';
+    }
+    for(y=1; y<=x; y++){
+      string += character + ' ';
+    };
+    string += '\n';
+};
 
+return string;
 };
 console.log(diamond_star('*'));
