2403b6f8310d540dc173125fe0515fd7171e1e22
Nurha <nurha@projectcodex.co>
2016-07-26 11:16:17 +0200

    finished function fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,24 @@
+// function fibonacci(number){
+//   var fibonacciSequence = [];
+//   for (x=0; x < number; x++) {
+//     fibonacciSequence((x < 2) ? x : fibonacciSequence[])
+//   }
+//   return fibonacciSequence;
+//
+//
+//
+// };
+// console.log(fibonacci(5));
+
+function fibonnaci(number) {
+  var sequence = [];
+  var num = 1;
+  for(x=0; x < number ; x++) {
+    sequence.push(num)
+    if(sequence.length > 1) {
+      num = num + sequence[x-1]
+    }
+  }
+  return sequence
+}
+ console.log(fibonnaci(100));
